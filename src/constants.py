0dd58bbae9220ec578518bce87b8f03744e67c34
dnlcrl <ciriello.daniele@gmail.com>
2015-12-16 03:07:45 +0100

    Next crew on ISS is @astro_tim @astro_timpeake & Yuri Malenchenko

--- src/constants.py
+++ src/constants.py
@@ -18,6 +18,8 @@ class Actions:
         retweet_astro_kjell = 'retweet_astro_kjell'
         retweet_astro_kimiya = 'retweet_astro_kimiya'
         retweet_volkov_iss = 'retweet_volkov_iss'
+        retweet_astro_tim = 'retweet_astro_tim'
+        retweet_astro_timpeake = 'retweet_astro_timpeake'
 
 
 class FileNames:
@@ -33,5 +35,7 @@ TWITTER_USERS = {
     Actions.TweetActions.retweet_scott_kelly: 'StationCDRKelly',
     Actions.TweetActions.retweet_astro_kjell: 'astro_kjell',
     Actions.TweetActions.retweet_astro_kimiya: 'astro_kimiya',
-    Actions.TweetActions.retweet_volkov_iss: 'volkov_iss'
+    Actions.TweetActions.retweet_volkov_iss: 'volkov_iss',
+    Actions.TweetActions.retweet_astro_tim: 'astro_tim',
+    Actions.TweetActions.retweet_astro_timpeake: 'astro_timpeake',
 }
